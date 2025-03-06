38cc3ee8584f76ef2638304a7038fb01f60c3a3b
Bensero87 <serhattumuklu@hotmail.com>
2025-03-06 23:47:13 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>LeakWhat - Veri Kontrol Platformu</h1>
-      <p>Bu sayfa, LeakWhat'ın başlangıç sayfasıdır.</p>
-    </div>
-  );
-}
