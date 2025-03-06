f109930d228f200ae21133522fbd4654e1f0515a
Bensero87 <serhattumuklu@hotmail.com>
2025-03-06 18:37:06 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div>
+      <h1>LeakWhat - Veri Kontrol Platformu</h1>
+      <p>Bu sayfa, LeakWhat'ın başlangıç sayfasıdır.</p>
+    </div>
+  );
+}
