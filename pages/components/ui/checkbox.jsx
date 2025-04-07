74e4e38c2e0abbbb9a87c7f9054a2bb6229cc23f
Bensero87 <serhattumuklu@hotmail.com>
2025-04-07 13:52:34 +0300

    Delete pages/components/ui directory

--- pages/components/ui/checkbox.jsx
+++
@@ -1,8 +0,0 @@
-export function Checkbox({ label, ...props }) {
-  return (
-    <label className="inline-flex items-center text-white">
-      <input type="checkbox" className="form-checkbox mr-2" {...props} />
-      {label}
-    </label>
-  );
-}
